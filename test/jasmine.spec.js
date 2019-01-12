d624c1f406cc793d7fb3ed0d879518d8870d2c30
Matteo Basso <mbasso96@gmail.com>
2019-01-12 13:58:52 +0100

    fix jasmine tests

--- test/jasmine.spec.js
+++ test/jasmine.spec.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import { JSDOM } from 'jsdom';
-import injectStyledUtils from '../src/jasmine';
 
 const dom = new JSDOM('<!doctype html><html><body></body></html>');
 global.window = dom.window;
 global.document = dom.window.document;
+global.navigator = dom.window.navigator;
+global.HTMLElement = dom.window.HTMLElement;
 
+const injectStyledUtils = require('../src/jasmine').default;
 const { Button, fadeIn, GlobalStyle } = require('./utils/framework');
 
 describe('jasmine', () => {
